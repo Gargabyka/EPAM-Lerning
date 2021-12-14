59b6c92d24b26d0f5aaf1b5b228af9d557d648d0
Alexsandr Larionov <alarionov0309@gmail.com>
2021-12-14 22:22:17 +0300

    Доработка 9 модуля. Добавлен 8 модуль

--- QueryModule9.sql
+++ QueryModule9.sql
@@ -4,3 +4,20 @@ dbo.Employees все не числоввые символы на любые чи
 UPDATE dbo.Employees SET PostalCode = CAST(100000 + (RAND(CHECKSUM(NEWID())) * 999999) as INT) 
 FROM dbo.Employees 
 WHERE PostalCode LIKE '%[^0-9]%'
+
+/*9.2. Построить план и оптимизировать запрос, представленный ниже, так 
+чтобы индекс индекс PostalCode работал не по табличному сканированию 
+(Index Scan), а по Index Seek. Необходимо пояснить, почему вы 
+оптимизировали запрос именно так*/
+
+SELECT EmployeeID
+FROM dbo.Employees
+WHERE PostalCode LIKE '98%';
+
+/*9.3. Разобраться с планом запроса, представленного ниже скрипта. 
+Оптимизировать запрос. Пояснить подробно почему вы считаете, что ваш 
+вариант оптимизации наиболее оптимизирует данный запрос и увеличит 
+его быстродействие?*/
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX Orders_OrderDate_Index 
+ON dbo.Orders(OrderDate,ShippedDate)
